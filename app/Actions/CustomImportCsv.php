0863690479d9634ee97e9dbbeeaf821f19deb15a
Eugene Wanjira <ujingene@users.noreply.github.com>
2022-04-30 16:19:47 +0300

    catch exceptions in cunstom

--- app/Actions/CustomImportCsv.php
+++ app/Actions/CustomImportCsv.php
@@ -16,28 +16,26 @@ class CustomImportCsv
 	}
 	public function execute($flag)
 	{
-        $flagname = Storage::path($flag->file_name);
-
-        //dd($flagname);
-
         // Count them, then grab them in chunks of 100.
-        $rows = $this->csvRowCount->execute($flagname);
-        $items_per_run = 100;
-        for ($i=0; $i <= $rows; $i = $i+$items_per_run+1) {
-        	$chunk = $this->csvSlice->execute($flagname, $i, $items_per_run);
-        	foreach ($chunk as $item) {
-        		echo "item stock code no = " .  $item->StockCode . "\n";
+        try{
+        	$flagname = Storage::path($flag->file_name);
+        	
+        	$rows = $this->csvRowCount->execute($flagname);
+	        $items_per_run = 100;
+	        for ($i=0; $i <= $rows; $i = $i+$items_per_run+1) {
+	        	$chunk = $this->csvSlice->execute($flagname, $i, $items_per_run);
+	        	foreach ($chunk as $item) {
+	        		echo "item stock code no = " .  $item->StockCode . "\n";
 
-        		$item->InvoiceDate = Carbon::parse($item->InvoiceDate)->toDateTimeString();
+	        		$item->InvoiceDate = Carbon::parse($item->InvoiceDate)->toDateTimeString();
 
-        		try {
-        			$invoice[] = Invoice::create((array) $item);
-        		} catch (\Exception $e)
-        		{
-        			return $e->getMessage();
-        		}
-        	}
-        }
+	        			$invoice[] = Invoice::create((array) $item);
+	        		} 
+	        	}
+	        } catch (\Exception $e)
+	        {
+				return $e->getMessage();
+			}
 
         return redirect()->route('csv_page');
 
