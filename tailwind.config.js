487c5bd39aa6b93f25f4165c205a105b2ca9ce6d
Eugene Wanjira <ujingene@users.noreply.github.com>
2022-04-30 15:38:16 +0300

    Configure app skeleton
    -  tailwind
    - maatwebsite/excel

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+  },
+  plugins: [
+  ],
+}
